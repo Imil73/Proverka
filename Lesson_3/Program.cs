a06a2cc29b7b22f3412415eee289b80212adfc81
Imil <Mil2163444@mail.ru>
2023-02-27 00:14:33 +0300

    Поиск квадрата

---
+++ Lesson_3/Program.cs
@@ -0,0 +1,12 @@
+﻿//1. По двум заданным числам проверять является ли первое квадратом второго
+
+Console.WriteLine("Введите предпологаемый квадрат числа: ");
+int square = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите само числo: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number * number == square)
+{
+System.Console.WriteLine($"{square} является квадратом {number}");}
+else
+{System.Console.WriteLine($"{square} не является квадратом {number}");
+}
