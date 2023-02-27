50f68eecb9034456eb6257e3caae78f48bf16854
Imil <Mil2163444@mail.ru>
2023-02-27 23:35:04 +0300

    6 задача

---
+++ Lesson_6/Program.cs
@@ -0,0 +1,10 @@
+﻿//6. Выяснить является ли число чётным
+System.Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number %2 == 0) 
+{
+    System.Console.WriteLine("Число чётное.");
+}
+else{
+    System.Console.WriteLine("Число не чётное.");
+}
